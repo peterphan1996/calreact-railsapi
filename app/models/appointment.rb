96a57ef82fad076305c99dcdd65cb89a57af29fa
peterphan1996 <peterphan_1996@live.com>
2017-07-29 16:42:40 +0700

    Chapter 9.4

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,7 +3,9 @@ class Appointment < ApplicationRecord
   validates_presence_of :title, :appt_time
   validates :title, length: {minimum: 3}
   validate :appt_time_cannot_be_in_the_past
+  belongs_to :user
 
+  
   private
   def appt_time_cannot_be_in_the_past
     if appt_time.present? && appt_time < Time.now
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-
+class User < ActiveRecord::Base
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :omniauthable
+  include DeviseTokenAuth::Concerns::User
+  has_many :appointments
 end
